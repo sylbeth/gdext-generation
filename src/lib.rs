fcd14b121d20d0990e2cb7bd8179b31f5bf41396
Sílvia Elisabeth Romero García <sylvielizshadow@gmail.com>
2024-12-05 17:55:37 +0100

    add default rust node

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+/// SVG representation of the default GDExtension Rust node.
+/// 
+/// # Author
+/// [burritobandit28](https://github.com/burritobandit28)
+/// 
+/// # License
+/// [CC BY 4.0 license](https://creativecommons.org/licenses/by/4.0/)
+const NODE_RUST: &str = include_str!("assets/NodeRust.svg");
