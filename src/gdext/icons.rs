42ef06c2dbe2463b2a55c56c83ca908b178041c2
Sílvia Elisabeth Romero García <sylvielizshadow@gmail.com>
2024-12-11 02:39:59 +0100

    implement storing of custom icons and icons found

--- src/gdext/icons.rs
+++ src/gdext/icons.rs
@@ -18,10 +18,58 @@ impl GDExtension {
     pub fn generate_icons(&mut self, icons_config: IconsConfig) -> Result<&mut Self> {
         let mut icons = Table::new();
 
+        let mut base_class_to_nodes = HashMap::<String, Vec<String>>::new();
+
+        find_children(&mut base_class_to_nodes)?;
+
+        for (icon, nodes) in base_class_to_nodes {
+            for node in nodes {
+                icons.insert(
+                    node,
+                    format!(
+                        "{PROJECT_FOLDER}{}.svg",
+                        (&icons_config.directories.base_directory)
+                            .join(&icons_config.directories.editor_directory)
+                            .join(&icon)
+                            .to_string_lossy()
+                    )
+                    .into());
+            }
+        }
+
+        if let Some(custom_icons) = &icons_config.custom_icons {
+            for (node, icon) in custom_icons {
+                icons.insert(
+                    node.clone(),
+                    format!(
+                        "{PROJECT_FOLDER}{}",
+                        (&icons_config.directories.base_directory)
+                            .join(&icons_config.directories.custom_directory)
+                            .join(icon)
+                            .to_string_lossy()
+                    )
+                    .into(),
+                );
+            }
+        }
+
         self.icons = Some(icons);
 
         Ok(self)
     }
 }
+
+/// Finds the structs that have inherited each base class, updating the base_class_to_nodes HashMap.
+///
+/// # Parameters
+///
+/// `base_class_to_nodes` - [`HashMap`] to fill with relationships `base_class: [struct1, ..., structn]`, of the structs that have inherited the base_class.
+///
+/// # Returns
+///
+/// * [`Ok`] - If the `base_class_to_nodes` [`HashMap`] could be filled.
+/// * [`Err`] - Otherwise.
+fn find_children(base_class_to_nodes: &mut HashMap<String, Vec<String>>) -> Result<()> {
     }
+    Ok(())
 }
